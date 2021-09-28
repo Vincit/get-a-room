0e7101933d94e976e5d1f66f5c8d7ba6c0ccd4cd
Mikko Pirhonen <mikko.m.pirhonen@tuni.fi>
2021-09-28 15:13:35 +0300

    Add city selection component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import logo from './../logo.svg';
 import './App.css';
+import CitySelection from './CitySelection';
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>Get A Room Testaan Pipelinen toimivuutta!!</p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React!
-                </a>
-            </header>
+        <div>
+            <CitySelection />
+            <div></div>
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>Get A Room Testaan Pipelinen toimivuutta!!</p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React!
+                    </a>
+                </header>
+            </div>
         </div>
     );
 }
