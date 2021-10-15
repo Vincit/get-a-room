70b9cd23a5236997492000cf44973555abbadd77
Mikko Pirhonen <mikko.m.pirhonen@tuni.fi>
2021-10-15 16:53:09 +0300

    Fix lint errors

--- backend/src/controllers/googleAPI/calendarAPI.ts
+++ backend/src/controllers/googleAPI/calendarAPI.ts
@@ -5,6 +5,8 @@ import * as schema from '../../utils/googleSchema';
 
 const calendar = google.calendar('v3');
 
+type NextEventById = Record<string, string | null | undefined>;
+
 /**
  * Run freeBusyQuery for items and return array containing objects
  * with id and start of next or current event
@@ -16,36 +18,32 @@ const calendar = google.calendar('v3');
  */
 export const freeBusyQuery = async (
     client: OAuth2Client,
-    items: any[],
+    items: schema.FreeBusyRequestItem[],
     start: string,
     end: string
-): Promise<any> => {
-    try {
-        const queryResult = await calendar.freebusy.query({
-            requestBody: {
-                timeMin: start,
-                timeMax: end,
-                items: items,
-                calendarExpansionMax: items.length
-            },
-            auth: client
-        });
+): Promise<NextEventById> => {
+    const queryResult = await calendar.freebusy.query({
+        requestBody: {
+            timeMin: start,
+            timeMax: end,
+            items: items,
+            calendarExpansionMax: items.length
+        },
+        auth: client
+    });
 
-        const calendars = queryResult.data.calendars;
-        const results: any = {};
+    const calendars = queryResult.data.calendars;
+    const results: NextEventById = {};
 
-        _.forIn(calendars, (data: schema.FreeBusyCalendar, id: string) => {
-            let startOfReservation: string | null | undefined = end;
+    _.forIn(calendars, (data: schema.FreeBusyCalendar, id: string) => {
+        let startOfReservation: string | null | undefined = end;
 
-            if (Array.isArray(data.busy) && data.busy.length !== 0) {
-                startOfReservation = data.busy[0].start;
-            }
+        if (Array.isArray(data.busy) && data.busy.length !== 0) {
+            startOfReservation = data.busy[0].start;
+        }
 
-            results[id] = startOfReservation;
-        });
+        results[id] = startOfReservation;
+    });
 
-        return results;
-    } catch (err) {
-        throw err;
-    }
+    return results;
 };
--- backend/src/utils/googleSchema.ts
+++ backend/src/utils/googleSchema.ts
@@ -4,3 +4,5 @@ export interface FreeBusyCalendar extends calendar_v3.Schema$FreeBusyCalendar {}
 export interface Building extends admin_directory_v1.Schema$Building {}
 export interface CalendarResource
     extends admin_directory_v1.Schema$CalendarResource {}
+export interface FreeBusyRequestItem
+    extends calendar_v3.Schema$FreeBusyRequestItem {}
