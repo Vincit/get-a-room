9631ba9282769a1d63f40a5ee921a341f82e4ece
Joona Prehti <joona.prehti@tuni.fi>
2021-10-26 09:10:50 +0300

    Added PRODUCTION_AUTH_URL and now on login the user is redirected to backend auth

--- frontend/src/components/GoogleLogin.tsx
+++ frontend/src/components/GoogleLogin.tsx
@@ -4,9 +4,14 @@ import './GoogleLogin.css';
 import image from './google_image.png';
 
 const DEVELOPMENT_AUTH_URL = 'http://localhost:8080/auth/google';
+const PRODUCTION_AUTH_URL = 'https://oispahuone.com/api/auth/google';
 
 function login() {
-    window.location.href = DEVELOPMENT_AUTH_URL;
+    if (process.env.NODE_ENV === 'development') {
+        window.location.href = DEVELOPMENT_AUTH_URL;
+    } else {
+        window.location.href = PRODUCTION_AUTH_URL;
+    }
 }
 
 const GoogleLogin = () => {
