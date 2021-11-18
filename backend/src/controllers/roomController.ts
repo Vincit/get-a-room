12f210f85aee01adb88f1d021699bf23a459fca0
Joonas Hiltunen <joonas.hiltunen@tuni.fi>
2021-11-18 22:00:05 +0200

    Add "until" query param to rooms

--- backend/src/controllers/roomController.ts
+++ backend/src/controllers/roomController.ts
@@ -72,9 +72,24 @@ export const fetchAvailability = () => {
                 return { id: x.id };
             });
 
-            // TODO: What should happen when the difference is e.g. 1 minute?
-            const start = DateTime.now().toUTC().toISO();
-            const end = DateTime.local().endOf('day').toUTC().toISO();
+            // Custom ending time
+            const customEnd = req.query.until as string;
+            let start, end;
+
+            // TODO: What should happen when the difference between start and end is small
+            if (customEnd) {
+                const startDt = DateTime.now().toUTC();
+                const endDt = DateTime.fromISO(customEnd).toUTC();
+                start = startDt.toISO();
+                end = endDt.toISO();
+
+                if (endDt <= startDt) {
+                    return responses.badRequest(req, res);
+                }
+            } else {
+                start = DateTime.now().toUTC().toISO();
+                end = DateTime.now().toUTC().endOf('day').toISO();
+            }
 
             // Combine all results from freeBusyQuery to this
             const results = {};
