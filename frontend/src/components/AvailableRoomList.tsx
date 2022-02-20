1eb9f99720c8834ed2c0af48a2d071d98fb6de23
Aki Lempola <aki.lempola@tuni.fi>
2022-02-20 12:31:08 +0200

    update available room list to new design

--- frontend/src/components/AvailableRoomList.tsx
+++ frontend/src/components/AvailableRoomList.tsx
@@ -1,60 +1,20 @@
 import React, { useState } from 'react';
 import {
-    Button,
     List,
-    Card,
-    CardActions,
-    CardContent,
     Typography,
-    IconButton,
-    Collapse,
     Box,
-    CircularProgress,
     Switch,
-    FormControlLabel
+    FormControlLabel,
 } from '@mui/material';
-import { Group, ExpandMore, ExpandLess } from '@mui/icons-material';
 import { makeBooking } from '../services/bookingService';
-import TimeLeft, { getTimeLeft } from './util/TimeLeft';
 import { Booking, BookingDetails, Room } from '../types';
 import useCreateNotification from '../hooks/useCreateNotification';
+import DurationPicker from './DurationPicker';
+import RoomCard from './RoomCard';
+import BookingDrawer from './BookingDrawer';
 
 function disableBooking(bookings: Booking[]) {
     return bookings.length === 0 ? false : true;
-}
-
-function getBookingTimeLeft(room: Room) {
-    let timeLeft = getTimeLeft(getNextCalendarEvent(room));
-    timeLeft = timeLeft.slice(0, -3);
-    return Number(timeLeft);
-}
-
-function getNextCalendarEvent(room: Room) {
-    return room.nextCalendarEvent;
-}
-
-function getName(room: Room) {
-    return room.name;
-}
-
-function getCapacity(room: Room) {
-    return room.capacity;
-}
-
-function getFeatures(room: Room) {
-    let features = room.features;
-    let featuresDisplay = [];
-
-    // Format room features
-    if (features) {
-        for (let i = 0; i < features.length; i++) {
-            featuresDisplay.push(features[i]);
-            if (i !== features.length - 1) {
-                featuresDisplay.push(', ');
-            }
-        }
-    }
-    return featuresDisplay;
 }
 
 type BookingListProps = {
@@ -70,40 +30,42 @@ const AvailableRoomList = (props: BookingListProps) => {
         useCreateNotification();
 
     const [bookingLoading, setBookingLoading] = useState('false');
-    const [expandedFeatures, setExpandedFeatures] = useState([] as string[]);
     const [expandedFeaturesAll, setExpandedFeaturesAll] = useState(
         false as boolean
     );
-    const [expandedBooking, setExpandedBooking] = useState('false');
-
-    const handleFeaturesCollapse = (room: Room) => {
-        if (expandedFeatures.includes(room.id)) {
-            // Collapse
-            expandedFeatures.forEach((element: any, index: any) => {
-                if (element === room.id) expandedFeatures.splice(index, 1);
-            });
-            setExpandedFeatures((expandedFeatures: any) => [
-                ...expandedFeatures
-            ]);
-        } else {
-            // Expand
-            setExpandedFeatures((expandedFeatures: any) => [
-                ...expandedFeatures,
-                room.id
-            ]);
-        }
-    };
+    const [expandBookingDrawer, setexpandBookingDrawer] = useState(false);
+    const [bookingDuration, setBookingDuration] = useState(15);
+    const [selectedRoom, setSelectedRoom] = useState<Room | undefined>(undefined);
 
     const handleAllFeaturesCollapse = () => {
         setExpandedFeaturesAll(!expandedFeaturesAll);
-        setExpandedFeatures([]);
     };
 
-    const handleBookingCollapse = (room: Room) => {
-        setExpandedBooking(expandedBooking === room.id ? 'false' : room.id);
-    };
+    const handleDurationChange = (duration: number) => {
+        setBookingDuration(duration);
+    }
 
-    const book = (room: Room, duration: number) => {
+    const handleReservation = () => {
+        book(selectedRoom, bookingDuration);
+        toggleDrawn(false);
+    }
+
+    const handleCardClick = (room: Room) => {
+        setexpandBookingDrawer(true);
+        setSelectedRoom(room);
+    }
+
+    const toggleDrawn = (newOpen: boolean) => {
+        if (newOpen === false) {
+            setSelectedRoom(undefined)
+        }
+        setexpandBookingDrawer(newOpen);
+    }
+
+    const book = (room: Room|undefined, duration: number) => {
+        if(room === undefined) { 
+            return; 
+        }
         let bookingDetails: BookingDetails = {
             duration: duration,
             title: 'Reservation from Get a Room!',
@@ -125,7 +87,18 @@ const AvailableRoomList = (props: BookingListProps) => {
             });
     };
     return (
-        <Box id="available-room-list" textAlign="center" pb={8}>
+        <Box id="available-room-list" textAlign="center" p={'16px'} >
+            <div id="drawer-container">
+            <BookingDrawer 
+                open={expandBookingDrawer} 
+                toggle={toggleDrawn}
+                bookRoom={handleReservation}
+                room={selectedRoom}
+                duration={bookingDuration}
+            />
+            </div>
+            
+            <DurationPicker onChange={handleDurationChange} />
             <FormControlLabel
                 label={
                     <Typography
@@ -143,226 +116,15 @@ const AvailableRoomList = (props: BookingListProps) => {
                 {rooms
                     .sort((a, b) => (a.name < b.name ? -1 : 1))
                     .map((room) => (
-                        <Card
-                            data-testid="AvailableRoomListCard"
-                            className="AvailableRoomListCardClass"
+                        <RoomCard 
                             key={room.id}
-                            sx={{
-                                background:
-                                    'linear-gradient(to right bottom, #c9c9c9, #969696)',
-                                backgroundColor: '#c9c9c9',
-                                border: 'success',
-                                borderRadius: 3,
-                                boxShadow: '5px 5px #bcbcbc',
-                                m: 2
-                            }}
-                        >
-                            <CardContent
-                                style={{
-                                    justifyContent: 'space-between',
-                                    display: 'flex',
-                                    textAlign: 'left'
-                                }}
-                            >
-                                <Box display="flex" flexDirection="column">
-                                    <Box
-                                        sx={{
-                                            display: 'flex',
-                                            justifyContent: 'left',
-                                            alignItems: 'center',
-                                            pt: 0.9
-                                        }}
-                                    >
-                                        <Group sx={{ pb: 0.3 }} />
-                                        <Typography
-                                            sx={{
-                                                fontSize: '18px',
-                                                fontWeight: 'bold'
-                                            }}
-                                        >
-                                            {getCapacity(room)}
-                                        </Typography>
-                                        <Typography
-                                            data-testid="BookingRoomTitle"
-                                            sx={{
-                                                fontSize: '18px',
-                                                fontWeight: 'bold',
-                                                ml: 1
-                                            }}
-                                        >
-                                            {getName(room)}
-                                        </Typography>
-                                    </Box>
-                                    <Box>
-                                        <TimeLeft
-                                            endTime={getNextCalendarEvent(room)}
-                                            timeLeftText="Free for: "
-                                        />
-                                    </Box>
-                                </Box>
-                                <Box>
-                                    <CardActions disableSpacing>
-                                        {!disableBooking(bookings) ? (
-                                            <Button
-                                                id="quickBook-button"
-                                                data-testid="QuickBookButton"
-                                                style={{
-                                                    backgroundColor: '#282c34',
-                                                    textTransform: 'none',
-                                                    color: 'white',
-                                                    fontSize: '16px',
-                                                    animation:
-                                                        'ripple 600ms linear',
-                                                    minWidth: '130px',
-                                                    minHeight: '50px',
-                                                    maxWidth: '130px',
-                                                    maxHeight: '50px'
-                                                }}
-                                                onClick={() =>
-                                                    handleBookingCollapse(room)
-                                                }
-                                                aria-label="Expand"
-                                            >
-                                                Quick Book
-                                                {expandedBooking === room.id ? (
-                                                    <ExpandLess />
-                                                ) : (
-                                                    <ExpandMore />
-                                                )}
-                                            </Button>
-                                        ) : null}
-                                    </CardActions>
-                                </Box>
-                            </CardContent>
-                            <CardContent
-                                style={{
-                                    justifyContent: 'space-between',
-                                    flexDirection: 'column',
-                                    display: 'flex',
-                                    textAlign: 'center'
-                                }}
-                            >
-                                {bookingLoading === room.id ? (
-                                    <Box
-                                        pb={2}
-                                        display="flex"
-                                        justifyContent="center"
-                                    >
-                                        <CircularProgress color="primary" />
-                                    </Box>
-                                ) : null}
-                                {expandedBooking === room.id ? (
-                                    <Collapse
-                                        in={expandedBooking === room.id}
-                                        timeout="auto"
-                                        unmountOnExit
-                                    >
-                                        <Box
-                                            sx={{
-                                                display: 'flex',
-                                                justifyContent: 'space-evenly',
-                                                mb: 2
-                                            }}
-                                        >
-                                            <Button
-                                                id="book30Min-button"
-                                                data-testid="Book30MinButton"
-                                                style={{
-                                                    backgroundColor: '#282c34',
-                                                    textTransform: 'none',
-                                                    color: 'white',
-                                                    fontSize: '16px',
-                                                    animation:
-                                                        'ripple 600ms linear',
-                                                    minWidth: '100px',
-                                                    minHeight: '50px',
-                                                    maxWidth: '120px',
-                                                    maxHeight: '50px'
-                                                }}
-                                                onClick={() => {
-                                                    book(room, 30);
-                                                    handleBookingCollapse(room);
-                                                }}
-                                            >
-                                                30 min
-                                            </Button>
-                                            {isNaN(getBookingTimeLeft(room)) ||
-                                            getBookingTimeLeft(room) >= 60 ? (
-                                                <Button
-                                                    id="book60Min-button"
-                                                    data-testid="Book60MinButton"
-                                                    style={{
-                                                        backgroundColor:
-                                                            '#282c34',
-                                                        textTransform: 'none',
-                                                        color: 'white',
-                                                        fontSize: '16px',
-                                                        animation:
-                                                            'ripple 600ms linear',
-                                                        minWidth: '100px',
-                                                        minHeight: '50px',
-                                                        maxWidth: '120px',
-                                                        maxHeight: '50px'
-                                                    }}
-                                                    onClick={() => {
-                                                        book(room, 60);
-                                                        handleBookingCollapse(
-                                                            room
-                                                        );
-                                                    }}
-                                                >
-                                                    60 min
-                                                </Button>
-                                            ) : null}
-                                        </Box>
-                                    </Collapse>
-                                ) : null}
-                                {getFeatures(room).length > 0 &&
-                                !expandedFeaturesAll ? (
-                                    <Box
-                                        style={{
-                                            display: 'flex',
-                                            justifyContent: 'center',
-                                            maxHeight: '10px'
-                                        }}
-                                    >
-                                        <CardActions disableSpacing>
-                                            <IconButton
-                                                data-testid="ExpansionButtonAvailableRoomList"
-                                                onClick={() =>
-                                                    handleFeaturesCollapse(room)
-                                                }
-                                                aria-label="Expand"
-                                            >
-                                                {expandedFeatures.includes(
-                                                    room.id
-                                                ) ? (
-                                                    <ExpandLess />
-                                                ) : (
-                                                    <ExpandMore />
-                                                )}
-                                            </IconButton>
-                                        </CardActions>
-                                    </Box>
-                                ) : null}
-                                <Collapse
-                                    in={
-                                        expandedFeatures.includes(room.id) ||
-                                        expandedFeaturesAll
-                                    }
-                                    timeout="auto"
-                                    unmountOnExit
-                                >
-                                    <Box mt={2}>
-                                        <Typography
-                                            style={{ fontSize: '16px' }}
-                                        >
-                                            {getFeatures(room)}
-                                        </Typography>
-                                    </Box>
-                                </Collapse>
-                            </CardContent>
-                        </Card>
+                            room={room}
+                            onClick={handleCardClick}
+                            bookingLoading={bookingLoading}
+                            disableBooking={disableBooking(bookings)}
+                            isSelected={selectedRoom===room}
+                            expandFeatures={expandedFeaturesAll}
+                        />
                     ))}
             </List>
         </Box>
--- frontend/src/theme.ts
+++ frontend/src/theme.ts
@@ -5,14 +5,101 @@ const ORANGE = '#f04e30';
 export const theme = createTheme({
     palette: {
         background: { default: '#f6f5f5' },
-        primary: { main: ORANGE }
+        primary: { main: ORANGE },
+        success: {main: '#219653'},
+        text: {
+            primary: '#443938',
+            secondary: ORANGE,
+            disabled: '#95807E',
+        },
+
     },
     typography: {
-        h3: { color: ORANGE },
+        h1: { color: ORANGE },
+        h2: { 
+            color: '#443938',
+            fontFamily: 'Proxima Nova, Roboto',
+            fontStyle: 'normal',
+            fontWeight: 'bold',
+            fontSize: '36px',
+            lineHeight: '36px',
+        },
+        h3: { 
+            color: '#443938',
+            fontFamily: 'Proxima Nova, Roboto',
+            fontStyle: 'normal',
+            fontWeight: 'bold',
+            fontSize: '24px',
+            lineHeight: '24px',
+        },
+        body1: {
+            fontFamily: 'Roboto',
+            fontStyle: 'normal',
+            fontWeight: 'normal',
+            fontSize: '16px',
+            lineHeight: '21px',
+        },
+        subtitle1: {
+            color: '#443938',
+            fontFamily: 'Proxima Nova, Roboto',
+            fontStyle: 'normal',
+            fontWeight: 'bold',
+            fontSize: '12px',
+            lineHeight: '12px',
+            textTransform: 'uppercase'
+        },
         h4: { color: ORANGE, fontWeight: 'bold' },
         h5: { color: ORANGE, fontWeight: 'bold' }
     },
     zIndex: {
         snackbar: 1
+    },
+    components: {
+        MuiToggleButton: {
+            styleOverrides: {
+                root: {
+                    textTransform: 'none',
+                    whiteSpace: 'nowrap',
+
+                    borderRadius: '50px',
+                    border: '1px solid #F04E30',
+                    boxSizing: 'border-box',
+                    padding: '8px 24px',
+
+                    color: '#F04E30',
+                    fontStyle: 'normal',
+                    fontWeight: 'bold',
+                    fontSize: '16px',
+                    lineHeight: '21px',
+                    
+                    '&.Mui-selected': {
+                        color: '#F6F5F5',
+                        background: '#F04E30'
+                    },
+                    '&.Mui-selected:hover': {
+                        background: '#C13217'
+                    }
+                },
+            }
+        },
+        MuiToggleButtonGroup: {
+            styleOverrides: {
+                root: {
+                    overflowX: 'scroll',
+                    display: 'flex',
+                    flexDirection: 'row',
+                    alignItems: 'flex-start',
+                    padding: '0 24px 0 0'
+
+                }
+            }
+        },
+        MuiCard: {
+            styleOverrides: {
+                root: {
+                    margin: '24px 0px'
+                }
+            }
+        }
     }
 });
