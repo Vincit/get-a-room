78890dd3ac0f21bf542cfa5082668f4f3b380045
juhonski <juho.karhunen@tuni.fi>
2022-02-22 19:02:48 +0200

    Edited preferences page
    Edited preferences page to look more like the office choosing page. Preferences page will be useless after all changes are done.

--- frontend/src/components/PreferencesView.tsx
+++ frontend/src/components/PreferencesView.tsx
@@ -1,3 +1,5 @@
+import * as React from 'react';
+
 import { Stack, Typography } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
@@ -7,6 +9,24 @@ import { Building, Preferences } from '../types';
 import BuildingSelect from './BuildingSelect';
 import CenteredProgress from './util/CenteredProgress';
 import FormButtons from './util/FormButtons';
+
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import FormGroup from '@mui/material/FormGroup';
+
+
+import GpsFixedIcon from '@mui/icons-material/GpsFixed';
+import SortByAlphaIcon from '@mui/icons-material/SortByAlpha';
+import { userInfo } from 'os';
+
+
+//Listaus
+
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+
+
 
 type PreferencesViewProps = {
     buildings: Building[];
@@ -18,6 +38,7 @@ const PreferencesView = (props: PreferencesViewProps) => {
     const { buildings, preferences, setPreferences } = props;
 
     const [selectedBuildingId, setSelecedBuildingId] = useState('');
+    const [alignment, setAlignment] = React.useState('proximity');
     const { createSuccessNotification, createErrorNotification } =
         useCreateNotification();
 
@@ -37,6 +58,15 @@ const PreferencesView = (props: PreferencesViewProps) => {
     const goToMainView = () => {
         history.push('/');
     };
+
+    const handleChange = (
+        event: React.MouseEvent<HTMLElement>,
+        newAlignment: string | null,
+      ) => {
+        if (newAlignment !== null) {
+            setAlignment(newAlignment);
+          }
+      };
 
     const handlePreferencesSubmit = () => {
         const foundBuilding = buildings.find(
@@ -63,10 +93,34 @@ const PreferencesView = (props: PreferencesViewProps) => {
             id="preferences-view"
             height="100%"
             justifyContent="space-around"
-        >
-            <Typography textAlign="center" variant="h3">
-                Preferences
-            </Typography>
+            >
+            <FormGroup>
+                <Typography textAlign="center" variant="h6">Welcome, asasdfasdfasdf</Typography>
+                <Typography textAlign="center" variant="h3">Choose office</Typography>
+            </FormGroup>
+
+            <ToggleButtonGroup
+            color="primary"
+            value={alignment}
+            exclusive
+            onChange={handleChange}
+            style={{alignSelf: "center"}}
+            >
+                
+                <ToggleButton style={{minWidth: '150px'}} value="proximity">
+                    <GpsFixedIcon style={{minWidth: '40px'}} fontSize='small'></GpsFixedIcon>
+                    Proximity
+                </ToggleButton>
+
+                <ToggleButton style={{minWidth: '150px'}} value="names">
+                    <SortByAlphaIcon style={{minWidth: '40px'}} fontSize='small'></SortByAlphaIcon>
+                    Names
+                </ToggleButton>
+            </ToggleButtonGroup>
+
+            
+
+
             <BuildingSelect
                 buildings={buildings}
                 selectedBuildingId={selectedBuildingId}
