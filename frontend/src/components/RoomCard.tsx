14f4510674eed488f23abe0fa8da7f6f46152854
gaurabmahat <gaurab.mahat.work@gmail.com>
2022-10-20 21:36:54 +0300

    changed Date dependencies to getBookingTimeLeft

--- frontend/src/components/RoomCard.tsx
+++ frontend/src/components/RoomCard.tsx
@@ -231,7 +231,7 @@ const RoomCard = (props: RoomCardProps) => {
     //a variable to store how much time is left on the booking.
     const duration = React.useMemo(() => {
         return getBookingTimeLeft(booking);
-    }, [Date, booking]);
+    }, [getBookingTimeLeft, booking]);
 
     const { createNotificationWithType } = useCreateNotification();
 
@@ -243,7 +243,7 @@ const RoomCard = (props: RoomCardProps) => {
                 'warning'
             );
         };
-        if (duration === 15) {
+        if (duration === 5) {
             showBookingEndNotification();
         }
     }, [duration]); //eslint-disable-line
