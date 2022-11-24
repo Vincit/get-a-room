31272331bbd87c78d936c28b68baca8eb9cad2b7
Jinhan Gao (TAU) <jinhan.gao@tuni.fi>
2022-11-24 21:00:03 +0200

    Make changes to user schema

--- backend/src/types/user.ts
+++ backend/src/types/user.ts
@@ -1,13 +1,14 @@
 import Preferences from '../types/preferences';
 import ScheduleData from '../types/scheduleData';
 import Subcription from '../types/subscription';
+import ScheduleDataArray from '../types/scheduleDataArray';
 
 type User = {
     subject: string;
     preferences: Preferences;
     name?: string;
     refreshToken?: string;
-    notification?: ScheduleData;
+    scheduleDataArray?: ScheduleDataArray;
     subscription?: Subcription;
 };
 
