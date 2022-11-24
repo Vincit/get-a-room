a02a2096ecd797568fd95bde4a89320144cd2025
Jinhan Gao (TAU) <jinhan.gao@tuni.fi>
2022-11-24 20:58:18 +0200

    Add array type

---
+++ backend/src/types/scheduleDataArray.ts
@@ -0,0 +1,13 @@
+import ScheduleData from '../types/scheduleData';
+
+/* type scheduleDataArray = Array<{
+    scheduleData?: ScheduleData | undefined;
+}>; */
+
+type scheduleDataArray = [
+    {
+        scheduleData: ScheduleData | undefined;
+    }
+];
+
+export default scheduleDataArray;
