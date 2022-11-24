65e21e98bcb53c8cb423bd38a70cdf7e37f03bdb
Jinhan Gao (TAU) <jinhan.gao@tuni.fi>
2022-11-24 21:00:49 +0200

    Delete old file

--- backend/src/models/notification.ts
+++
@@ -1,9 +0,0 @@
-import { Schema } from 'mongoose';
-import subcription from '../types/subscription';
-import scheduleData from '../types/scheduleData';
-import { type } from 'os';
-
-export const scheduleDataSchema = new Schema<scheduleData>({
-    endTime: { required: true, type: String },
-    notification: { required: true, type: Object }
-});
