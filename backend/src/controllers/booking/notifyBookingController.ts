8cac6c3c92b70d107acf69292d1a6270706fe047
Jinhan Gao (TAU) <jinhan.gao@tuni.fi>
2022-12-02 17:59:35 +0200

    Add schedule funtions

--- backend/src/controllers/booking/notifyBookingController.ts
+++ backend/src/controllers/booking/notifyBookingController.ts
@@ -10,16 +10,20 @@ import webpush from 'web-push';
 import {
     updateSubscription,
     updateScheduleData,
-    getUserWithSubject
+    getUserWithSubject,
+    getScheduleDataArrayId
 } from '../userController';
 import _ from 'lodash';
 import scheduleDataArray from '../../types/scheduleDataArray';
+import { scheduleDataSchema } from '../../models/scheduleData';
 
 // PublicKey adn privateKey
 
 const publicKey: string =
     'BEvPpDPDB543o1VH8QsnHJC2BW2znZqip3KJ4kxtFR98zetTY4TSozQIWllDfV8bnyZoQP_XCfuYC1G0C5WUNgU';
 const privateKey: string = '2sy8ts8Z7yXDnB2E5EZhwfx3Y7nXcJRnhgT12SgDVOA';
+
+webpush.setVapidDetails('mailto:test@test.com', publicKey, privateKey);
 
 /**
  * Receive and store the subscription
@@ -140,28 +144,80 @@ export const scheduleNotification = () => {
         res: Response,
         next: NextFunction
     ) => {
-        const endTime: String = res.locals.endTime;
-
-        const minute: String = endTime.split(':')[1];
-        const hour: String = endTime.split(':')[0].split('T')[1];
-
-        const minuteN: Number = Number(minute) - 14;
-        const minute2: String = String(minuteN);
-
-        const scheduleTime = '*' + minute2 + hour + '***';
         try {
-            const job = schedule.scheduleJob('id', scheduleTime, () => {
+            const endTime: String = res.locals.endTime;
+
+            const minute: String = endTime.split(':')[1];
+            const hour: String = endTime.split(':')[0].split('T')[1];
+
+            const minuteN: Number = Number(minute) - 5;
+            const minute2: String = String(minuteN);
+
+            const scheduleTime = '*' + minute2 + hour + '***';
+
+            const sub = res.locals.sub;
+            const scheduleData: ScheduleData = res.locals.scheduleData;
+
+            const options = {
+                vapidDetails: {
+                    subject: 'mailto:test@test.com',
+                    publicKey: publicKey,
+                    privateKey: privateKey
+                },
+                TTL: 60
+            };
+
+            if (!sub) {
+                return responses.badRequest(req, res);
+            }
+            const user = await getScheduleDataArrayId(sub, scheduleData);
+            //const uniqueId:string = user?.scheduleDataArray?;
+
+            const job = schedule.scheduleJob('uniqueId', scheduleTime, () => {
                 const subscription = res.locals.subscription;
                 const payload = JSON.stringify({
                     title: 'Meeting End Notification',
                     body: 'Your current meeting is going to an end in 5 minutes!'
                 });
-                webpush.sendNotification(subscription, payload);
+                webpush.sendNotification(subscription, payload, options);
             });
 
             if (!job) {
                 return responses.internalServerError(req, res);
             }
+
+            next();
+        } catch (err) {
+            next(err);
+        }
+    };
+
+    return middleware;
+};
+
+/**
+ * Cancel a scedule job if user cancel a meeting
+ * @returns
+ */
+export const cancelSceduleJob = () => {
+    const middleware = async (
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ) => {
+        try {
+            const sub = res.locals.sub;
+            const scheduleData: ScheduleData = res.locals.scheduleData;
+
+            if (!sub) {
+                return responses.badRequest(req, res);
+            }
+
+            const user = await getScheduleDataArrayId(sub, scheduleData);
+            //const uniqueId:string = user?.scheduleDataArray;
+
+            const scheduleJob = schedule.scheduledJobs['_id'];
+            scheduleJob.cancel();
 
             next();
         } catch (err) {
