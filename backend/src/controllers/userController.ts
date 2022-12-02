b55d78077495ac2a8266ac7e3c31c8d78c04e7d7
Jinhan Gao (TAU) <jinhan.gao@tuni.fi>
2022-12-02 18:05:14 +0200

    Add funtion to search schdeule data

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -56,3 +56,13 @@ export function updateScheduleData(
         { $push: { scheduleDataArray: scheduleData } }
     ).exec();
 }
+
+export function getScheduleDataArrayId(
+    subject: string,
+    scheduleData: ScheduleData
+): Promise<User | null> {
+    return UserModel.findOne([
+        { subject },
+        { scheduleDataArray: scheduleData }
+    ]).exec();
+}
