5e2f573302749e8ec78cd0c4035799d1079683e8
NhiTran <nhi.tran@futurice.com>
2022-12-02 21:00:44 +0200

    feat: UI part for defining time freely feature

--- frontend/src/components/BookingDrawer/BookingDrawer.tsx
+++ frontend/src/components/BookingDrawer/BookingDrawer.tsx
@@ -198,6 +198,8 @@ const BookingDrawer = (props: Props) => {
     const [nextHalfHour, setNextHalfHour] = useState('00:30');
     const [nextFullHour, setNextFullHour] = useState('01:00');
 
+    const currentTime = DateTime.now();
+
     const handleAdditionalTime = (minutes: number) => {
         onAddTime(minutes);
     };
